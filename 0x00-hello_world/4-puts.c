43ae8d40d5f8d9f0afdc2cd54bf1c1cbc1ac769c
Francis Nwuko <frankdspecific@gmail.com>
2022-09-08 15:01:45 -0700

    Task 4 puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle in the put function
+ *
+ * Return: 0 if properly executed, non zero if otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
