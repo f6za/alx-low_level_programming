41768a812a7485856ed70563bbf7e1948e3b14b6
Francis Nwuko <frankdspecific@gmail.com>
2022-09-08 15:48:23 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints "Programming is like building a multilingual puzzle in the put function
- * Return: 0 if properly executed, non zero if otherwise
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
