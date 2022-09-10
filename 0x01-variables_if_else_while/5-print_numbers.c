ba99e1a9c4ce1fe552f4bf47a78c12e1645ecd93
Francis Nwuko <frankdspecific@gmail.com>
2022-09-09 19:39:47 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - print if the number is postive, zero, or negative
+ * main - prints all single digit numbers of base
  *
- * Description: using the main function
- * this program prints "Programming is positive, zero, or negative
- * Return: 0
+ * Description: 10 starting from 0, followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 
 {
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
-{
-	putchar(ch);
-}
-putchar('\n');
-return (0);
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
 }
