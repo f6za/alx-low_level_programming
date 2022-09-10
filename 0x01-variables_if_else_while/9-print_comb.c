eb7feae6b6cbbd98056e549ee40739d95bae3083
Francis Nwuko <frankdspecific@gmail.com>
2022-09-09 20:29:45 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of single-digit numbers
+ * main - printing numbers from 0-9 with commas and space between them
  *
- * Description: Return: Always 0 (Success)
+ * Description: using the main function
+ * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
+ * Return: 0
  */
 int main(void)
 {
-	int n;
+	int c;
 
-	for (n = 48; n < 58; n++)
+	for (c = 48; c <= 57; c++)
 	{
-		putchar(n);
-		if (n != 57)
+		putchar(c);
+		if (c != 57)
 		{
 			putchar(',');
 			putchar(' ');
