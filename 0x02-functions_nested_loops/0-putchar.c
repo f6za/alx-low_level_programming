4394e6ba49959c9e67b96ae105dac0bfff4a89af
Francis Nwuko <frankdspecific@gmail.com>
2022-09-13 13:00:06 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * main - prints _putchar
+ * main - print _putchar
  *
  * Description: prints _putchar
  *
- * Return: 0 (Success)
+ * Return: 0 (success)
  */
 
-Int main (void)
+int main(void)
 {
 	char text[9] = "_putchar";
-	int i = 0;
+		int i = 0;
 
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(text[i]);
-	}
+		for (i = 0; i < 8; i++)
+		{			
+			_putchar(text[i]);
+		}
 	_putchar('\n');
 
-	Return (0);
+	return (0);
 }
