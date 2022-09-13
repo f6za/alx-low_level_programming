b7dd48e7594cca99715767190ad74c649cced9cc
Francis Nwuko <frankdspecific@gmail.com>
2022-09-13 14:24:54 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,9 +5,7 @@
  *
  * Return: always 0
  */
-
 int main(void)
-
 {
 	_putchar('_');
 	_putchar('p');
@@ -18,7 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 
 	return (0);
 }
