e02ad5bb740a6ea6c619454af8c6f28c1fc31d98
Francis Nwuko <frankdspecific@gmail.com>
2022-09-13 14:58:04 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,9 +2,9 @@
 
 /**
  * main - print putchar
- *
  * Return: always 0
  */
+
 int main(void)
 {
 	_putchar('_');
@@ -18,4 +18,5 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
+
 }
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-This README lists out some programs that prints certain functions on C
+This is my readme for C program scripts
