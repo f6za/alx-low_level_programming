3761ea7891948e3fc9fad8ed7ada06a7bff016a4
Francis Nwuko <frankdspecific@gmail.com>
2022-09-15 02:18:32 -0700

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,9 +9,9 @@ int main(void)
 {
 int i;
 
-	printf("Infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 
-	i = 0;	
+i = 0;	
  /*
   * while (i < 10)
   * {
@@ -19,7 +19,8 @@ int i;
   * }
   */
 
-	printf("Infinite loop avoided! \\o/\n");
+printf("Infinite loop avoided! \\o/\n");
 
-	return (0);
+return (0);
+
 }
